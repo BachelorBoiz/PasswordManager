2c4dcf863e20a95064ed39621f5768586ae19786
julianswagger <juli55d3@easv365.dk>
2023-10-09 09:08:08 +0200

    Adding some things
    PasswordHasher
    IPasswordHasher
    PasswordManager.UnitTests
    PasswordHasherTests

---
+++ PasswordManager.Infrastructure/PasswordHasher.cs
@@ -0,0 +1,49 @@
+﻿using PasswordManager.Core.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Infrastructure
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        private const int saltSize = 128 / 8;
+        private const int KeySize = 256 / 8;
+        private const int Iterations = 10000;
+        private static readonly HashAlgorithmName _hashAlgorithmName = HashAlgorithmName.SHA256;
+        private static char Delimiter = ';';
+
+        public string Hash(string password)
+        {
+            PasswordNullException(password);
+
+            var salt = RandomNumberGenerator.GetBytes(saltSize);
+            var hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, _hashAlgorithmName, KeySize);
+
+            return string.Join(Delimiter, Convert.ToBase64String(salt), Convert.ToBase64String(hash));
+        }
+
+        public bool Verify(string passwordHash, string inputPassword)
+        {
+            PasswordNullException(inputPassword);
+
+            var elements = passwordHash.Split(Delimiter);
+            var salt = Convert.FromBase64String(elements[0]);
+            var hash = Convert.FromBase64String(elements[1]);
+            var inputHash = Rfc2898DeriveBytes.Pbkdf2(inputPassword, salt, Iterations, _hashAlgorithmName, KeySize);
+
+            return CryptographicOperations.FixedTimeEquals(hash, inputHash);
+        }
+
+        private void PasswordNullException(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                throw new ArgumentException("Password cannot be null or empty.", nameof(password));
+            }
+        }
+    }
+}
---
+++ PasswordManager.UnitTests/PasswordHasherTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PasswordManager.Core.Abstractions;
+using PasswordManager.Infrastructure;
+
+namespace PasswordManager.UnitTests
+{
+    public class PasswordHasherTests
+    {
+        private readonly PasswordHasher _passwordHasher;
+
+        public PasswordHasherTests() {
+            _passwordHasher = new PasswordHasher();
+        }
+        
+        
+        [Fact]
+        public void PasswordHasher_Hash_NullInput()
+        {
+            // Arrange
+            string inputPassword = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => _passwordHasher.Hash(inputPassword));
+        }
+
+        [Fact]
+        public void PasswordHasher_Hash_EmptyInput()
+        {
+            // Arrange
+            string inputPassword = string.Empty;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => _passwordHasher.Hash(inputPassword));
+        }
+
+        [Fact]
+        public void PasswordHasher_Verify_NullInput()
+        {
+            // Arrange
+            string inputPassword = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => _passwordHasher.Verify("Hashed Password inside DB", inputPassword));
+        }
+
+        [Fact]
+        public void PasswordHasher_Verify_EmptyInput()
+        {
+            // Arrange
+            string inputPassword = string.Empty;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => _passwordHasher.Verify("Hashed Password inside DB", inputPassword));
+        }
+
+
+        [Fact]
+        public void PasswordHasher_Verify_VerifyWorks() {
+            //Arrange
+            string password = "ZuckerMusk!9/11";
+            string hashedPassword = _passwordHasher.Hash(password);
+            //Act
+            bool isPasswordValid = _passwordHasher.Verify(hashedPassword, password);
+            //Assert
+            Assert.True(isPasswordValid);
+
+        }
+    }
+}
